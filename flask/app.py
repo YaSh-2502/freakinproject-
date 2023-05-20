c828794c377d84d1324b0df5ef471141903f8bf7
Narasima Pandiyan <35830653+NarasimaPandiyan@users.noreply.github.com>
2023-05-21 00:15:32 +0530

    using some jinja stuff. Need to create some seperate pages for cars and blog.

--- flask/app.py
+++ flask/app.py
@@ -4,8 +4,16 @@ import os
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route("/aboutUs")
+def aboutUs():
+    return render_template('aboutUs.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
 
 @app.route('/static_files')
 def static_files():
